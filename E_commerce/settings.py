cb88388fb8056116de0c711e60c4af18ecf8b7e8
Junior Ruiz <junior31064049@gmail.com>
2022-03-18 10:57:50 -0400

    Personalizando en admin

--- E_commerce/settings.py
+++ E_commerce/settings.py
@@ -32,14 +32,19 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'admin_interface',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'app',
+    'app.apps.AppConfig',
+    'colorfield',
 ]
+
+X_FRAME_OPTIONS = 'SAMEORIGIN'
+SILENCED_SYSTEM_CHECKS = ["security.W019"]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- app/admin.py
+++ app/admin.py
@@ -1,7 +1,14 @@
+from re import search
 from django.contrib import admin
 from .models import Marca, Producto
 
 # Register your models here.
 
+class ProductoAdmin(admin.ModelAdmin):
+    list_display = ["nombre", "precio", 'marca', 'fecha_fabricacion']
+    search_fields = ["nombre"]
+    list_filter = ["marca", "fecha_fabricacion"]
+    list_per_page = 10
+
 admin.site.register(Marca)
-admin.site.register(Producto)
+admin.site.register(Producto, ProductoAdmin)
